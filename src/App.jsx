93ea397e47a012257d3b2e045924ebdaad810c71
shahddwekat <shahdenad0@gmail.com>
2026-06-19 21:53:51 +0300

    Scroll down fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import MatchPage from './pages/MatchPage'
 import SchedulePage from './pages/SchedulePage'
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="bg-chalk min-h-screen">
       <Header />
+      <ScrollToTop />
 
       <Routes>
         <Route path="/" element={<Home />} />
